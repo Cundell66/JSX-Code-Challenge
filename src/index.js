008170b128a833d354243f91c8e4d9182d6f5a4f
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-14 15:24:59 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Chips</li>
+      <li>Chocolate</li>
+      <li>Steak</li>
+    </ul>
+  </div>,
+
+  document.getElementById("root")
+);
+
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
